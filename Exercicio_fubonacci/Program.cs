d0a277d3a30bc5ed542c84d537af131f6875f206
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-05 15:11:46 -0300

    Add project files.

---
+++ Exercicio_fubonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //fibonacci 
+
+        int n1=1, n2=0, aux, position;
+        string t = "";
+
+        Console.WriteLine("digite a posicao do fibonacci: ");
+        position = int.Parse(Console.ReadLine());
+
+        
+        if (position >= 1)
+        {
+            for (int i = 0; i < position; i++)
+            {
+                aux = n2;
+                n2 += n1;
+                n1 = aux;
+
+                t += n2+" ";
+            }
+            Console.WriteLine(t);
+        }
+        else
+        {
+            Console.WriteLine("nao contempla");
+        }
+    }
+}
